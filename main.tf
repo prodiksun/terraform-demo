bf1df5ffe85b4fef533574cb177fa9a87127a609
prodiksun <prodik.sun@gmail.com>
2022-09-29 14:07:21 +0300

    7

--- main.tf
+++ main.tf
@@ -1,11 +1,46 @@
 terraform {
   required_providers {
+    # see https://registry.terraform.io/providers/hashicorp/random
+    random = {
+      source = "hashicorp/random"
+      version = "3.1.0"
+    }
+    # see https://registry.terraform.io/providers/hashicorp/template
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
+    }
+    # see https://github.com/dmacvicar/terraform-provider-libvirt
     libvirt = {
       source = "dmacvicar/libvirt"
+      version = "0.6.3"
     }
   }
 }
 
 provider "libvirt" {
-  # Configuration options
+  uri = "qemu:///system"
+}
+
+# определяем переменные для домена  
+resource "libvirt_domain" "test" {
+  name   = "test"
+  memory = "1024"
+  vcpu   = 1
+
+  network_interface {
+    network_name = "default"
+  }
+
+  console {
+    type = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
 }
